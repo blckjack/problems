939d9f92d22cb1b8b16da72641d6e3f5e67bdf32
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-14 19:04:12 +0200

    try to solve the problem

--- chess-no-fight-zone-2.py
+++ chess-no-fight-zone-2.py
@@ -1,10 +1,6 @@
-#deck = [[0 for x in range(8)] for y in range(8)]
-
-
-
-Q = (1, 3)
-T = (2, 4)
-K = (1, 6)
+Q = (4, 1)
+T = (4, 3)
+K = (5, 5)
 
 def countNoFightZone():
     N = 0
@@ -13,28 +9,147 @@ def countNoFightZone():
         y = 1
         while y < 9:
             cc = (x, y)
-            if not checkQueen(cc):
-                N = N + 1
+            if checkQueen(Q, cc):
+                N += 1
+            if checkKnight(K, cc):
+                N += 1
+            if checkTower(T, cc):
+                N += 1
             y = y + 1
         x = x + 1
     print N
 
 
-def checkQueen(cc):
-    rule = []
-    while ***:
-        rule.append[()]
-    if cc in rule: 
+def checkQueen(Q, cc):
+    if checkBishop(Q, cc) == True or checkTower(Q, cc) == True:
         return True
+    else:
+        return False
 
-def checkTower(cc):
-    """
-    check that tower can reach B from A
-    return T or F
-    """
-    pass
+def checkTower(T, cc):
+    x = T[0]
+    y = T[1]
 
-def checkKnight(cc):
-    pass
+    while y < 8:
+        y += 1
+        if x == cc[0] and y == cc[1]:
+            return True
+    x = T[0]
+    y = T[1]
+    while x < 8:
+        x += 1
+        if x == cc[0] and y == cc[1]:
+            return True
+    x = T[0]
+    y = T[1]
+    while 1 < y:
+        y -= 1
+        if x == cc[0] and y == cc[1]:
+            return True
+    x = T[0]
+    y = T[1]
+    while 1 < x:
+        x -= 1
+        if x == cc[0] and y == cc[1]:
+            return True
+
+def checkBishop(B, cc):
+
+    x = B[0]
+    y = B[1]
+    while y < 8 and x < 8:
+        y += 1
+        x += 1
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = B[0]
+    y = B[1]
+    while y < 8 and 0 < x:
+        y += 1
+        x -= 1
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = B[0]
+    y = B[1]
+    while 0 < y and x < 8:
+        y -= 1
+        x += 1
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = B[0]
+    y = B[1]
+    while 0 < y and 0 < x:
+        y -= 1
+        x -= 1
+        if x == cc[0] and y == cc[1]:
+            return True
+
+def checkKnight(K, cc):
+
+    x = K[0]
+    y = K[1]
+    if y < 8 and x < 8:
+        x +=2 
+        y +=1
+        if x == cc[0] and y == cc[1]:
+            return True
+    x = K[0]
+    y = K[1]
+    if y < 8 and x < 8:
+        x +=1 
+        y +=2
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = K[0]
+    y = K[1]
+    if 0 < y and x < 8:
+        x +=1 
+        y -=2
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = K[0]
+    y = K[1]
+    if 0 < y and x < 8:
+        x +=2 
+        y -=1
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = K[0]
+    y = K[1]
+    if y < 8 and 0 < x:
+        x -=2 
+        y +=1
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = K[0]
+    y = K[1]
+    if y < 8 and 0 < x:
+        x -=1 
+        y +=2
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = K[0]
+    y = K[1]
+    if 0 < x and 0 < x:
+        x -=1 
+        y -=2
+        if x == cc[0] and y == cc[1]:
+            return True
+
+    x = K[0]
+    y = K[1]
+    if 0 < x and 0 < x:
+        x -=2 
+        y -=1
+        if x == cc[0] and y == cc[1]:
+            return True
 
 countNoFightZone()

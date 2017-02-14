1975ee26fc407f9e1231eea241c8dbe647852030
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-14 20:27:10 +0200

    review fixes

--- chess-no-fight-zone-2.py
+++ chess-no-fight-zone-2.py
@@ -8,59 +8,55 @@ def countNoFightZone():
     while x < 9:
         y = 1
         while y < 9:
-            cc = (x, y)
-            if checkQueen(Q, cc) and checkQueen(Q, cc) != checkKnight(K, cc) and checkQueen(Q, cc) != checkTower(T, cc):
-                N += 1
-            if checkKnight(K, cc) and checkKnight(K, cc) != checkQueen(Q, cc) and checkKnight(K, cc) != checkTower(T, cc):
-                N += 1
-            if checkTower(T, cc) and checkTower(T, cc) != checkQueen(Q, cc) and checkTower(T, cc) != checkKnight(K, cc):
+            target = (x, y)
+            if checkKnight(K, target) or checkQueen(Q, target) or checkTower(T, target):
                 N += 1
             y = y + 1
         x = x + 1
     print N
 
 
-def checkQueen(Q, cc):
-    if checkBishop(Q, cc) == True or checkTower(Q, cc) == True:
+def checkQueen(Q, target):
+    if checkBishop(Q, target) == True or checkTower(Q, target) == True:
         return True
     else:
         return False
 
-def checkTower(T, cc):
+def checkTower(T, target):
     x = T[0]
     y = T[1]
 
     while y < 8:
         y += 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
     x = T[0]
     y = T[1]
     while x < 8:
         x += 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
     x = T[0]
     y = T[1]
     while 1 < y:
         y -= 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
     x = T[0]
     y = T[1]
     while 1 < x:
         x -= 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
 
-def checkBishop(B, cc):
+def checkBishop(B, target):
 
     x = B[0]
     y = B[1]
     while y < 8 and x < 8:
         y += 1
         x += 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
 
     x = B[0]
@@ -68,7 +64,7 @@ def checkBishop(B, cc):
     while y < 8 and 0 < x:
         y += 1
         x -= 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
 
     x = B[0]
@@ -76,7 +72,7 @@ def checkBishop(B, cc):
     while 0 < y and x < 8:
         y -= 1
         x += 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
 
     x = B[0]
@@ -84,72 +80,36 @@ def checkBishop(B, cc):
     while 0 < y and 0 < x:
         y -= 1
         x -= 1
-        if x == cc[0] and y == cc[1]:
+        if (x, y) == target:
             return True
 
-def checkKnight(K, cc):
+def checkKnight(K, target):
+    x = K[0]
+    y = K[1]
+    
+    if (x+2, y+1) == target:
+        return True
 
-    x = K[0]
-    y = K[1]
-    if y < 8 and x < 8:
-        x +=2 
-        y +=1
-        if x == cc[0] and y == cc[1]:
-            return True
-    x = K[0]
-    y = K[1]
-    if y < 8 and x < 8:
-        x +=1 
-        y +=2
-        if x == cc[0] and y == cc[1]:
-            return True
+    if (x+1, y+2) == target:
+        return True
 
-    x = K[0]
-    y = K[1]
-    if 0 < y and x < 8:
-        x +=1 
-        y -=2
-        if x == cc[0] and y == cc[1]:
-            return True
+    if (x+1, y-2) == target:
+        return True
 
-    x = K[0]
-    y = K[1]
-    if 0 < y and x < 8:
-        x +=2 
-        y -=1
-        if x == cc[0] and y == cc[1]:
-            return True
+    if (x+2, y-1) == target:
+        return True
 
-    x = K[0]
-    y = K[1]
-    if y < 8 and 0 < x:
-        x -=2 
-        y +=1
-        if x == cc[0] and y == cc[1]:
-            return True
+    if (x-2, y+1) == target:
+        return True
 
-    x = K[0]
-    y = K[1]
-    if y < 8 and 0 < x:
-        x -=1 
-        y +=2
-        if x == cc[0] and y == cc[1]:
-            return True
+    if (x-1, y+2) == target:
+        return True
 
-    x = K[0]
-    y = K[1]
-    if 0 < x and 0 < x:
-        x -=1 
-        y -=2
-        if x == cc[0] and y == cc[1]:
-            return True
+    if (x-1, y-2) == target:
+        return True
 
-    x = K[0]
-    y = K[1]
-    if 0 < x and 0 < x:
-        x -=2 
-        y -=1
-        if x == cc[0] and y == cc[1]:
-            return True
+    if (x-2, y-1) == target:
+        return True
+    return False
 
 countNoFightZone()

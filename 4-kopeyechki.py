f38ffe508a4f7998448906a311dcf72baa34a5fb
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-15 16:04:51 +0200

    4-add-line to remove first list item

--- 4-kopeyechki.py
+++ 4-kopeyechki.py
@@ -1,7 +1,8 @@
-# http://acmp.ru/asp/do/index.asp?main=task&id_problem=11
+# http://acmp.ru/asp/do/index.asp?main=topic&id_course=2&id_section=10&id_topic=3
 import sys
 
 items = open('INPUT.txt').read().splitlines()
+del items[0]
 i1 = items.count('1')
 i2 = items.count('0')
 

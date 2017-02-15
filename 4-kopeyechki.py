667f13b39bb9ea5e5018291d2e5cfb4c4f961da6
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-15 18:56:19 +0200

    fix kopeechka txt

--- 4-kopeyechki.py
+++ 4-kopeyechki.py
@@ -1,10 +1,18 @@
 # http://acmp.ru/asp/do/index.asp?main=topic&id_course=2&id_section=10&id_topic=3
 import sys
+f_in = open("INPUT.txt")
+N = [int(s) for s in f_in.readline().strip().split()]
+AA = [int(s) for s in f_in.readline().strip().split()]
 
-items = open('INPUT.txt').read().splitlines()
-del items[0]
-i1 = items.count('1')
-i2 = items.count('0')
+i = 0
+i1 = 0
+i2 = 0
+while i < N[0]:
+    if AA[i] == 1:
+        i1 += 1
+    else:
+        i2 += 1
+    i += 1
 
 
 def rotateKops(i1, i2):

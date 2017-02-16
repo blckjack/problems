8385b0c40e7557eba8e512dd9827a1d96d89d26a
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-16 14:20:44 +0200

    #8 - solved (probably)

---
+++ 8-turnaround.py
@@ -0,0 +1,11 @@
+# http://acmp.ru/asp/do/index.asp?main=task&id_course=2&id_section=10&id_topic=4&id_problem=16
+f_in = open("INPUT.txt")
+N = [int(s) for s in f_in.readline().strip().split()]
+AA = [int(s) for s in f_in.readline().strip().split()]
+
+AAA = []
+i = N[0] - 1
+while i > -1:
+    AAA.append(str(AA[i]))
+    i -= 1
+print (' '.join(AAA))

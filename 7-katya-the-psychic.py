f6eee6c9643cea2798859ec1e336f1817e34ae85
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-16 18:03:37 +0200

    try to solve #9, wrong result RLY???

--- 7-katya-the-psychic.py
+++ 7-katya-the-psychic.py
@@ -1,5 +1,1 @@
 # http://acmp.ru/asp/do/index.asp?main=task&id_course=2&id_section=10&id_topic=3&id_problem=14
-f_in = open("INPUT.txt")
-N = [int(s) for s in f_in.readline().strip().split()]
-
-print (N)

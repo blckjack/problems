f687ced0636d2ae0ec6073cc8cd01956db473023
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-17 12:54:38 +0200

    #7 solved

--- 7-katya-the-psychic.py
+++ 7-katya-the-psychic.py
@@ -1,1 +1,19 @@
 # http://acmp.ru/asp/do/index.asp?main=task&id_course=2&id_section=10&id_topic=3&id_problem=14
+f_in = open("INPUT.txt")
+sp = [int(s) for s in f_in.readline().strip().split()]
+
+
+y = 0
+S = sp[0]
+P = sp[1]
+while y <= 1000:
+    if P == y * (S - y):
+        x = int(P/y)
+        if S == x + y:
+            if x > y:
+                print(y, x)
+                break
+            if x <= y:
+                print(x, y)
+                break
+    y += 1

e5b67fd772be0d0f6a4c2ca364e14c661a8b4f15
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-28 17:24:34 +0200

    #20 - wedding - solved. but 'runtime error' appeareed :((((

---
+++ 020-gorgon-wedding.py
@@ -0,0 +1,23 @@
+f_in = open("INPUT.txt")
+n = int(f_in.readline().strip())
+bridegrooms_wealth = [int(s) for s in f_in.readline().strip().split()]
+gorgons_wealth = int(f_in.readline().strip())
+
+i = 0
+while i < n:
+    k = i
+    poorest_wealth = 100000000
+    while k < n:
+        if bridegrooms_wealth[k] < poorest_wealth:
+            poorest_wealth = bridegrooms_wealth[k]
+            poorest_bridegroom = k
+        k += 1
+    tmp = bridegrooms_wealth[i]
+    bridegrooms_wealth[i] = poorest_wealth
+    bridegrooms_wealth[poorest_bridegroom] = tmp
+
+    if gorgons_wealth < bridegrooms_wealth[i]:
+        gorgons_wealth = (gorgons_wealth + bridegrooms_wealth[i]) / 2
+    i += 1
+
+print(format(gorgons_wealth, '.6f'))

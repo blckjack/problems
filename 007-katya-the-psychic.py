d55eaed9ecc48bcc955a15528b0bba3c5d79dd1a
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-02-28 16:20:54 +0200

    007 - refactored

--- 007-katya-the-psychic.py
+++ 007-katya-the-psychic.py
@@ -1,23 +1,26 @@
 # http://acmp.ru/asp/do/index.asp?main=task&id_course=2&id_section=10&id_topic=3&id_problem=14
 f_in = open("INPUT.txt")
 sp = [int(s) for s in f_in.readline().strip().split()]
-
 S = sp[0]
 P = sp[1]
+
+# more efficient way to solve:
 x = 1
-y = S//2
-while x < S//2:
+while x < S//2 + 1:
+    y = S//2 + 1
     while y < S:
-        if P == x * y and S == x + y:
+        if P == x * y:
             if x > y:
                 print(y, x)
                 break
             if x <= y:
                 print(x, y)
                 break
-        print(x,y)
+        y += 1
     x += 1
 
+# alpha ver.:
+#
 # while y <= 1000:
 #     if P == y * (S - y):
 #         x = int(P/y)

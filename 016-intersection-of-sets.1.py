27c6394f0be51e74a3e05acf5830b32cde22b199
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-03-07 01:01:04 +0200

    #16 - refactored, but second test is failed. need assist.

--- 016-intersection-of-sets.1.py
+++ 016-intersection-of-sets.1.py
@@ -3,29 +3,31 @@ f_in = open("INPUT.txt")
 n = [int(s) for s in f_in.readline().strip().split()]
 first_sequence = [int(s) for s in f_in.readline().strip().split()]
 second_sequence = [int(s) for s in f_in.readline().strip().split()]
-final_sequence = first_sequence + second_sequence
-count = []
+dump = [0]*300000
+result = []
 
-# create empty array
+# create origin from first_sequence
 i = 0
-while i < 10:
-    count.append(0)
+while i < n[0]:
+    f = first_sequence[i]
+    dump[f] = 1
     i += 1
 
-# merge arrays, and create a single sequence
+# compare origin and second_sequence
 i = 0
-while i < len(second_sequence):
-    first_sequence.append(second_sequence[i])
+while i < n[1]:
+    s = second_sequence[i]
+    if dump[s] == 1:
+        result.append(s)
     i += 1
 
-# find the repeated values
+# sort result ->
 i = 0
-while i < len(first_sequence):
-    k = 0
-    while k < len(first_sequence):
-        if first_sequence[i] == k:
-            count[k] += 1
-        k += 1
+while i < len(result) - 1:
+    if result[i] > result[i+1]:
+        p = result[i+1]
+        result[i+1] = result[i]
+        result[i] = p
     i += 1
 
-print(first_sequence, count)
+print(" ".join(map(str, result)))

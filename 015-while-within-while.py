3eacb4ef81e9f92a9f476c8e84b1fced9e267401
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-03-07 01:51:21 +0200

    #15 - refactored - solved

--- 015-while-within-while.py
+++ 015-while-within-while.py
@@ -1,24 +1,35 @@
 f_in = open("INPUT.txt")
 n = [int(s) for s in f_in.readline().strip().split()]
+'INPUT: 1 5 5 10'
 
-# build sequence A:
-A = []
-i = n[0]
-while i <= n[1]:
-    A.append(i)
-    i += 1
-# build sequence B:
-B = []
-k = n[2]
-while k <= n[3]:
-    B.append(k)
-    k += 1
-# build compared sequence z:
-ii = 0
-while ii < len(A):
-    kk = 0
-    while kk < len(B):
-        z = int(str(A[ii]) + str(B[kk]))
-        kk += 1
-        print(z,)
-    ii += 1
+i = 0
+while n[i] < n[2]:
+    k = n[2]
+    while k < n[3] + 1:
+        print(str(n[i]) + str(k))
+        k += 1
+    n[i] += 1
+
+
+# # build sequence A:
+# A = []
+# i = n[0]
+# while i <= n[1]:
+#     A.append(i)
+#     i += 1
+# # build sequence B:
+# B = []
+# k = n[2]
+# while k <= n[3]:
+#     B.append(k)
+#     k += 1
+# # build compared sequence z:
+# ii = 0
+# while ii < len(A):
+#     kk = 0
+#     while kk < len(B):
+#         z = int(str(A[ii]) + str(B[kk]))
+#         kk += 1
+#         print(z,)
+#     ii += 1
+

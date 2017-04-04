3ebb4fbc4677974f39c2c03f52fe1054f59e6537
Nikita Goncharenko <nikita.goncharenko@grammarly.com>
2017-04-04 19:45:12 +0300

    #33 - done

--- 033-saddle-points.py
+++ 033-saddle-points.py
@@ -3,8 +3,6 @@ f_in = open("INPUT.txt")
 n = [int(s) for s in f_in.readline().strip().split()]
 N = n[0]
 M = n[1]
-a = []
-b = []
 result = []
 
 i = 0
@@ -14,64 +12,47 @@ while i < N:
     i += 1
 
 
-def minimumLineValue():
+def main():
+    count = 0
     i = 0
     while i < N:
         k = 0
-        minimum = 10000000
         while k < M:
-            if result[i][k] < minimum:
-                minimum = result[i][k]
-                min_i = [i, k]
-            k += 1
-
-            k = 0
-            while k < N:
-                i = 0
-                while i < M:
-                    if minimum == result[k][i]:
-                        b.append([k, i])
-                    i += 1
-                k += 1
-        i += 1
-    print(b)
-
-
-def maximumColumnValue():
-    k = 0
-    while k < M:
-        i = 0
-        maximum = -1
-        while i < N:
-            if result[i][k] > maximum:
-                maximum = result[i][k]
-                max_i = [i, k]
-            i += 1
-
-            i = 0
-            while i < N:
-                k = 0
-                while k < M:
-                    if maximum == result[i][k]:
-                        a.append([i, k])
-                    k += 1
-                i += 1
-        k += 1
-    print(a)
-
-
-def iterateThroughMap():
-    minimumLineValue()
-    maximumColumnValue()
-    count = 0
-    i = 0
-    while i < len(b):
-        k = 0
-        while k < len(a):
-            if b[i] == a[k]:
+            if find_minimal_value(i) == find_maximum_value(k):
                 count += 1
             k += 1
         i += 1
     print(count)
 
-iterateThroughMap()
+
+def find_minimal_value(i):
+    k = 0
+    j = 0
+    while k < M:
+        while j < M:
+            if result[i][k] > result[i][j]:
+                tmp = result[i][j]
+                result[i][j] = result[i][k]
+                result[i][k] = tmp
+            j += 1
+        k += 1
+        k = j
+    return result[i][0]
+
+
+def find_maximum_value(k):
+    i = 0
+    j = 0
+    while i < M:
+        while j < M:
+            if result[i][k] > result[j][k]:
+                tmp = result[j][k]
+                result[j][k] = result[i][k]
+                result[i][k] = tmp
+            j += 1
+        i += 1
+        i = j
+    return result[-1][k]
+
+
+main()
